b64364cef2264a340921d4409521946fec726a22
thanh phú <htp1912@gmail.com>
2023-11-08 23:26:55 +0700

    add ckfinder sample

---
+++ src/main/java/com/laptrinhjavaweb/IndexController.java
@@ -0,0 +1,15 @@
+package com.laptrinhjavaweb;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @RequestMapping("/sample")
+    public void index(HttpServletResponse response) {
+        // Redirect to CKFinder's samples.
+        response.setHeader("Location", "/ckfinder/static/samples/index.html");
+        response.setStatus(302);
+    }
+}
